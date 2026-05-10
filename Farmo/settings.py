86eac8988f767b60f527b7d156c80c6b0a20387f
vaibhav <vaibhavnagargoje2000@gmail.com>
2026-05-10 20:13:19 +0530

    increase refresh token to 90 days

--- Farmo/settings.py
+++ Farmo/settings.py
@@ -244,9 +244,9 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60), # Token lasts 1 hour
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),    # Refresh works for 7 days
-    # 'ROTATE_REFRESH_TOKENS': True,  
-    # 'BLACKLIST_AFTER_ROTATION': True,
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),   # Refresh works for 90 days (3 months)
+    'ROTATE_REFRESH_TOKENS': True,  
+    'BLACKLIST_AFTER_ROTATION': False,
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
